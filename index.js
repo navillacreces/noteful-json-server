1b24927fc839966729a9cf939d684ae334fbf45f
Nicholas Villacreces <nvillacreces72@gmail.com>
2020-04-22 13:21:03 -0500

    package audit + 10 steps

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ const server = jsonServer.create()
 const router = jsonServer.router(db())
 const middlewares = jsonServer.defaults()
 
+const PORT = process.env.PORT || 9090;
+
 server.use(middlewares)
 server.use(router)
 
-server.listen(9090, () => {
-  console.log('Noteful json-server started at http://localhost:9090')
+server.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+server.listen(PORT, () => {
+  console.log(`Noteful json-server started at ${PORT}`)
 })
